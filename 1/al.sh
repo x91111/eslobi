962d4c489948bd7a51b59d5b8088b49583b63ae1
Aziz <x91111@gmail.com>
2014-01-12 03:35:15 +0300

    Create al.sh

---
+++ 1/al.sh
@@ -0,0 +1,24 @@
+
+### 0
+#z يفتح الملف
+
+
+alias "z"="kate ~/al.sh"
+alias "ط"="kate ~/al.sh"
+
+
+# 1
+
+
+alias "س"="zsh"
+alias "سم"="alias"
+alias "قل"="echo"
+alias "اكتب"="vim"
+alias "اقرا"="cat"
+alias "اكتب عربي"="vim -A"
+alias "اعد"="zsh"
+alias "نظف"="clear"
+alias "التاريخ"="clear"
+
+alias "التحديث"="agg"
+
